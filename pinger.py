02d9ec793a451053b03442a9dd7e7b9d6a2d70a8
aghaseyed1 <aghaseyed7@gmail.com>
2022-09-17 18:18:24 +0430

    change method of exit

--- pinger.py
+++ pinger.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-import socket
-import sys
-import threading
+from socket import socket, AF_INET, SOCK_STREAM
+from os import _exit
+from sys import argv
+from threading import Thread
 from notifypy import Notify
 
 host = '132.145.109.138'
 port = 8002
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client = socket(AF_INET, SOCK_STREAM)
 
 
 def notif(msg):
@@ -69,13 +70,13 @@ def pinger():
                 print("unknown message: ", message)
                 client.close()
                 print("connection close")
-                sys.exit(1)
+                _exit(1)
 
         except Exception as e:
             print("An error occured!")
             print(e)
             client.close()
-            sys.exit(1)
+            _exit(1)
 
 
 def send_nick(nick):
@@ -93,14 +94,14 @@ def write():
 
 if __name__ == "__main__":
     # Check args
-    if len(sys.argv) == 1:
+    if len(argv) == 1:
         print("use: ./pinger YOUR_NICKNAME")
-        sys.exit(1)
-    nickname = sys.argv[1]
+        _exit(1)
+    nickname = argv[1]
 
     # Thread definition
-    write_thread = threading.Thread(target=write)
-    receive_thread = threading.Thread(target=pinger)
+    write_thread = Thread(target=write)
+    receive_thread = Thread(target=pinger)
 
     # Connect to server
     client = connect(nickname)
